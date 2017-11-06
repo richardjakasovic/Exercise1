537edd5b9dbe08adcaae3c7d5f5d2f1a54ab6155
Richard <Richard@DESKTOP-EFMSNEU.Speedport_W_724V_Typ_A_05011603_05_020>
2017-11-06 16:42:07 +0100

    HelloUser is added

---
+++ src/HelloUser.java
@@ -0,0 +1,11 @@
+
+public class HelloUser {
+
+	public HelloUser(String userName) {
+		
+	}
+	
+	public void greetUser(String userName) {
+		System.out.print("Hello " + userName + "!");
+	}
+}
--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,9 +1,19 @@
+import java.util.Scanner;
 
 public class HelloWorld {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println("Hello ASE2017 - How are you? :)");
+		
+		System.out.print("Plese enter your name: ");
+		
+		Scanner in = new Scanner(System.in);
+		String userName = in.nextLine();
+		
+		HelloUser hu = new HelloUser(userName);
+		hu.greetUser(userName);
+		
 	}
 
 }
